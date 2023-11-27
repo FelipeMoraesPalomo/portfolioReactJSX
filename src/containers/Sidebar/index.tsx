04d01bf7856c87f4d601ff8be2ee81b79ca5b852
Felipe <felipemoraespalomo@gmail.com>
2023-11-27 18:19:01 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/felipemoraespalomo.png" />
+    <Titulo fontSize={20}>Felipe Moraes Palomo</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const GithubSecao = styled.div`
+  margin-top: 32px;
+  margin-bottom: 64px;
+
+  img {
+    height: 157px;
+    max-width: 50%;
+
+    @media (max-width: 768px) {
+      max-width: 100%;
+      width: 100%;
+    }
+  }
+`
