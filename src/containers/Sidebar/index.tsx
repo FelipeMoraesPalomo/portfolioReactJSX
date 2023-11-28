07ca4dede741a9feccd67500b062dc901903047f
Felipe <felipemoraespalomo@gmail.com>
2023-11-28 19:00:03 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/felipemoraespalomo.png" />
-    <Titulo fontSize={20}>Felipe Moraes Palomo</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Felipe Moraes Palomo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        FelipeMoraesPalomo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Futuro Fullstack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
